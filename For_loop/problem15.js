f96156009fe6f3bb4ef01cec339d4548d00256f6
Taton Dhara <tdhara657@gmail.com>
2023-02-06 10:51:31 +0530

    Day 6 code

---
+++ For_loop/problem15.js
@@ -0,0 +1,6 @@
+for(let i=1;i<=10;i++){
+  if(i==6){
+    continue;
+  }
+  console.log(i)
+}
---
+++ For_loop/problem16.js
@@ -0,0 +1,7 @@
+let sum=0;
+let count=0;
+for(let i=1;i<=100;i++){
+  sum+=i;
+  count++
+}
+console.log(sum/count)
